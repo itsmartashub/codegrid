52600aa3451e81ebe32f11ad5e9ef7092e260edd
master <coderawii@gmail.com>
2023-05-23 23:36:27 +0200

    [website-reveal-marquee-animation]: Website Reveal Animation | Marquee Animation CSS | Landing Page HTML, CSS & GSAP
    // TODO:
     - pronaci adekvatne fontove

---
+++ website-reveal-marquee-animation/js/app.js
@@ -0,0 +1,17 @@
+gsap.from('.clip-top, .clip-bottom', { duration: 2, delay: 1, height: '50vh', ease: 'power4.inOut' })
+
+gsap.to('.marquee', { duration: 3.5, delay: 0.75, top: '50%', ease: 'power4.inOut' })
+
+gsap.from('.clip-top .marquee, .clip-bottom .marquee', { duration: 5, delay: 1, left: '100%', ease: 'power3.inOut' })
+
+gsap.from('.clip-center .marquee', { duration: 5, delay: 1, left: '-50%', ease: 'power3.inOut' })
+
+gsap.to('.clip-top', { duration: 2, delay: 6, clipPath: 'inset(0 0 100% 0)', ease: 'power4.inOut' })
+gsap.to('.clip-bottom', { duration: 2, delay: 6, clipPath: 'inset(100% 0 0 0)', ease: 'power4.inOut' })
+
+gsap.to('.loader__clip .marquee, .clip-center .marquee span', {
+	duration: 1,
+	delay: 6,
+	opacity: 0,
+	ease: 'power2.inOut',
+})
